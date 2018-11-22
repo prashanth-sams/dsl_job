35237896cb0099c034ab5abc0ac26bc38e8518f2
prashanth-sams <sams.prashanth@gmail.com>
2018-11-22 17:04:56 +0400

    manual parallel job 5

--- jenkins-dsl/test-jobs/pipeline_parallel.groovy
+++ jenkins-dsl/test-jobs/pipeline_parallel.groovy
@@ -2,17 +2,20 @@ multiJob("pipeline_parallel") {
 	description("multi phase job inside a multi phase job")
 
 	steps {
-		phase("Parallel Test") {
+		phase("Parallel Test 1") {
 			phaseJob("allure_behave_new") {
 				killPhaseCondition("NEVER")
 			}
 			phaseJob("allure_behave_old") {
 				killPhaseCondition("NEVER")
 			}
-			phase("Parallel Test 2") {
-				phaseJob("allure_behave") {
-					killPhaseCondition("NEVER")
-				}
+
+			continuationCondition("ALWAYS")
+			executionType("PARALLEL")
+		}
+		phase("Parallel Test 2") {
+			phaseJob("allure_behave") {
+				killPhaseCondition("NEVER")
 			}
 
 			continuationCondition("ALWAYS")
