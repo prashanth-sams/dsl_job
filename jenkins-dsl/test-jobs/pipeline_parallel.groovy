1dde77de91fc53784db90f22b5e733bfb171450a
prashanth-sams <sams.prashanth@gmail.com>
2018-11-22 16:01:16 +0400

    manual parallel job 2

---
+++ jenkins-dsl/test-jobs/pipeline_parallel.groovy
@@ -0,0 +1,15 @@
+buildTrigger = build("manual_parallel")
+
+description("multi phase job inside a multi phase job")
+parallel(
+	{
+		parallel(
+			{
+				buildA = build("allure_behave_latest")
+			},
+			{
+				buildB = build("allure_behave_old")
+			}
+		)
+	}
+)
