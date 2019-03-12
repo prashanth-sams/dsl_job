952b9a2e23b4edffb90a08f5e2608b8a9617f464
prashanth-sams <sams.prashanth@gmail.com>
2019-03-12 18:05:17 +0400

    asdasd

--- jenkins-dsl/test-jobs/pipe_launch.groovy
+++ jenkins-dsl/test-jobs/pipe_launch.groovy
@@ -3,20 +3,17 @@ pipelineJob('pipe_launch_new') {
         cps {
             script("""
             node {
-              try {
-                  stage('allure_behave') {
-                    build(job: 'allure_behave')
-                    def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
-                    desktop_build = desktop_job_data.getLastBuild().result.toString()
-                    echo (desktop_job_data.getLastBuild().result.toString())
-                  }
-              } catch(e) {
-                  def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
-                  desktop_build = desktop_job_data.getLastBuild().result.toString()
-                  echo (desktop_job_data.getLastBuild().result.toString())
-                  echo (desktop_job_data.getLastBuild().getNumber().toString())
-                  echo e.toString()
-              }
+try {
+  stage('allure_behave') {
+    build(job: 'allure_behave')
+    def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
+    desktop_build = desktop_job_data.getLastBuild().result.toString()
+  }
+} catch(e) {
+  def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
+  desktop_build = desktop_job_data.getLastBuild().result.toString()
+  echo e.toString()
+}
 
               try {
                   stage('allure_behave_latest') {
