ec3549ee7189f8c961f59dcabd1b9c04f396a54f
prashanth-sams <sams.prashanth@gmail.com>
2019-03-12 17:46:52 +0400

    yay

--- jenkins-dsl/test-jobs/pipe_launch.groovy
+++ jenkins-dsl/test-jobs/pipe_launch.groovy
@@ -3,9 +3,19 @@ pipelineJob('pipe_launch_new') {
         cps {
             script("""
               node {
-                stage('allure_behave') {
-                  build(job: 'allure_behave')
-                  echo 'hello'
+                try {
+                    stage('allure_behave') {
+                      build(job: 'allure_behave')
+                      def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
+                      desktop_build = desktop_job_data.getLastBuild().result.toString()
+                      echo (desktop_job_data.getLastBuild().result.toString())
+                    }
+                } catch(e) {
+                    def desktop_job_data = Jenkins.instance.getItemByFullName("allure_behave")
+                    desktop_build = desktop_job_data.getLastBuild().result.toString()
+                    echo (desktop_job_data.getLastBuild().result.toString())
+                    echo (desktop_job_data.getLastBuild().getNumber().toString())
+                    echo e.toString()
                 }
               }
             """)
