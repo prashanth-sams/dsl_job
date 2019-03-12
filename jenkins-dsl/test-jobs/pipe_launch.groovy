c2bbd4506e4b63d6d2e57df39301e2f9a846698b
prashanth-sams <sams.prashanth@gmail.com>
2019-03-12 17:45:02 +0400

    add

--- jenkins-dsl/test-jobs/pipe_launch.groovy
+++ jenkins-dsl/test-jobs/pipe_launch.groovy
@@ -2,11 +2,12 @@ pipelineJob('pipe_launch_new') {
     definition {
         cps {
             script("""
-              stage('allure_behave') {
-                build(job: 'allure_behave')
-                echo 'hello'
+              node {
+                stage('allure_behave') {
+                  build(job: 'allure_behave')
+                  echo 'hello'
+                }
               }
-
             """)
             sandbox()
         }
